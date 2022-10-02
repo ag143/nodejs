0c263602531f6f817183f42f465940e05727bfa1
ag143 <ag14341@gmail.com>
2022-10-03 01:26:56 +0530

    Update index.js

--- workingone/node-test/index.js
+++ workingone/node-test/index.js
@@ -15,6 +15,18 @@ var server = http.createServer(function (req, resp) {
              
             resp.end();
         });
+    }else if (req.url === "/overview") {
+        fs.readFile("overview/index.html", function (error, pgResp) {
+            if (error) {
+                resp.writeHead(404);
+                resp.write('Contents you are looking are Not Found');
+            } else {
+                resp.writeHead(200, { 'Content-Type': 'text/html' });
+                resp.write(pgResp);
+            }
+             
+            resp.end();
+        });
     } else {
         //4.
         resp.writeHead(200, { 'Content-Type': 'text/html' });
